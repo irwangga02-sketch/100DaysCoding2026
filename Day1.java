66095c92946ebd22687577bc6156548f8921ff0f
irwangga02-sketch <irwangga02@gmail.com>
2026-09-02 21:02:23 +0800

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,13 @@
+// langkah pertama kita membuat class bernama Day1 //
+// langkah kedua kita tambahkan {} yang menandai awal dan akhir blok kode //
+// langkah ketiga kita tambahkan method main,dimana method ini yang pertama kali dijalankan oleh java //
+// langkah keempat kita tambah System.out.println() dan tanda parentheses/() bisa kita isi parameter atau nilai yang akan kita cetak //
+// langkah terakhir menambahkan ; untuk menandai akhir dari sebuah statement //
+
+public class Day1{
+  public static void main(String[] args) {
+    System.out.println("hello world");
+  }
+}
+
+
