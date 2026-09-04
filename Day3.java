f86efd555a061c56460207a6634bcc7dc093f5a1
irwangga02-sketch <irwangga02@gmail.com>
2026-09-04 18:30:32 +0900

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,26 @@
+public class Day3 {
+    public static void main(String[] args) {
+      
+        //1. \n - newline = membuat teks berpindah kebaris berikutnya
+        System.out.println("==data mahasiswa==\n1.nama : irwangga\n2.umur : 20 thun\n3.prodi : informatika ");
+        
+        //2. \t - tab = memberi jarak seperti tombol tab   
+        System.out.println("nama\t: irwangga");
+        System.out.println("jurusan\t: informatika");
+        
+        //3. \" - double quote = untuk menampilkan tanda petik 2 (")
+        System.out.println("isc : \"berantas buta coding\"");
+        
+        //4. \' - single quote = untuk menampilkan tanda petik 1 (')
+        System.out.println("isc : \'berantas buta coding\' ");
+
+        //5. \r - carriage return = untuk kembali ke awal baris
+        System.out.println("jaya\runsulbar");
+
+        //6. \\ - backslash = menampilkan backslash(\)
+        System.out.println("c:\\xampp\\htdocs\\pertemuan_1");
+
+        //7. \b - backspace = untuk bergerak satu karakter ke belakang
+        System.out.println("ABCD\bDurian");
+     }
+    }
