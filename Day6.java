b0f1ae90287b1ad8f08b2b61c6bd9002ee440db8
irwangga02-sketch <irwangga02@gmail.com>
2026-09-07 21:09:51 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,12 @@
+public class Day6 {
+  public static void main(String[] args) {
+    
+    char kelamin = 'L'; 
+    // tipe data char,hanya bisa diisi dengan satu angka,huruf dan simbol
+    // ukuran char yakni 2 byte
+
+    boolean lulus = true;
+    // tipe data boolean adalah tipe data yang hanya bisa diisi dengan true dan false
+    // ukuran boolean yakni 1 byte
+  }
+}
