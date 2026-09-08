5597fa82efa0a07a19357032e5d85600e773d2ec
irwangga02-sketch <irwangga02@gmail.com>
2026-09-08 16:51:01 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,13 @@
+public class Day7 {
+    
+    public static void main(String[] args) {
+        String nama = "jokowi";
+        /* String adalah tipe data yang dapat menampung teks
+           string ditandai dengan parameter/nilai punya tanda petik 2
+           " " seperti diatas "jokowi"  jika tidak diberi
+           akan eror
+        */
+          System.out.println("presiden terbaik : "+ nama);
+    }
+    
+}
