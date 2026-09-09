f00ac838c9a8deb5ceeca65f44ff2d90d023f97a
irwangga02-sketch <irwangga02@gmail.com>
2026-09-09 19:43:05 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,11 @@
+public class Day8 {
+    
+    public static void main(String[] args) {
+        int nilaiujian;//membuat variabel yang nilainya/parameternya bisa diubah
+        nilaiujian = 75;
+        System.out.println("nilai sebelum mengulang : "+ nilaiujian);
+        nilaiujian = 85;
+        System.out.println("nilai setelah mengulang : "+nilaiujian);
+    }
+    
+}
