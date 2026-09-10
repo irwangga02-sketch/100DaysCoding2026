52fca8d1b894e39a6ccb6b22ffd64d39a3312771
irwangga02-sketch <irwangga02@gmail.com>
2026-09-10 23:08:06 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,14 @@
+public class Day9 {
+    
+    public static void main(String[] args) {
+        final String NAMA = "joko"; 
+        /* constanta variabel yang nilainya
+         * nggak bisa diubah lagi setelah diisi 
+         * pertama kali
+         * variabelnya juga memakai final diawal
+         * seperti contoh diatas
+         * 
+         */
+    }
+    
+}
