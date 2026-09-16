91f0d21a4a617deb7943fb55724db7e9c43dad45
irwangga02-sketch <irwangga02@gmail.com>
2026-09-16 21:42:00 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,14 @@
+public class Day15 {
+    
+    public static void main(String[] args) {
+        int angka1 = 4;
+        int angka2 = 4;
+        
+        int perkalian = angka1 * angka2;
+        int pembagian = angka1 / angka2;
+        
+        System.out.println("hasil perkalian angka1 dan angka2 : "+ perkalian);
+        System.out.println("hasil pembagian angka1 dan angka2 : "+ pembagian);
+    }
+    
+}
