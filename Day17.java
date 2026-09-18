72b06677f97ca82b95554530788c3afdc3203068
irwangga02-sketch <irwangga02@gmail.com>
2026-09-18 17:36:54 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,23 @@
+public class Day17 {
+    
+    public static void main(String[] args) {
+        int nilaiAwal = 5;
+        System.out.println("nilai Awal : "+ nilaiAwal);
+      
+        nilaiAwal += 10;
+        System.out.println("hasil += 10 : "+ nilaiAwal);
+        
+        nilaiAwal -= 1;
+        System.out.println("hasil -= 1 : "+ nilaiAwal);
+        
+        nilaiAwal *= 2;
+        System.out.println("hasil *= 2 : "+ nilaiAwal);
+        
+        nilaiAwal /= 4;
+        System.out.println("hasil /= 4 : "+ nilaiAwal);
+        
+        nilaiAwal %= 2;
+        System.out.println("hasil %= 2 : "+ nilaiAwal);
+    }
+    
+}
