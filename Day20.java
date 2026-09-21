6e06e954d50fae3033748dad8c9230411186e92f
irwangga02-sketch <irwangga02@gmail.com>
2026-09-21 22:12:31 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,14 @@
+public class Day20 {
+    
+    public static void main(String[] args) {
+        int umur = 20;
+        String s1 = umur + "hihiihi"; 
+        System.out.println(s1);
+      
+        char inisial = 'p';
+        String hasil = String.valueOf(inisial);
+        System.out.println("Inisial: " + hasil);
+        
+     }
+    
+   }
