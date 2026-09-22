5f1df72bcd1a1a91d3557343bcde2105671cd93a
irwangga02-sketch <irwangga02@gmail.com>
2026-09-22 22:19:59 +0800

    Create Day21.java

---
+++ Day21.java
@@ -0,0 +1,7 @@
+public class Day21 { 
+    public static void main(String[] args) {
+        String kata = "25";
+        int angka = Integer.parseInt(kata);
+        System.out.println("hasil : "+ angka);   
+     }    
+   }
