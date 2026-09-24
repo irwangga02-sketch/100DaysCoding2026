69d56c29426746b4a902a168e5e40047ba8192c9
irwangga02-sketch <irwangga02@gmail.com>
2026-09-24 21:42:38 +0800

    Create Day23.java

---
+++ Day23.java
@@ -0,0 +1,10 @@
+public class Day23 {
+    
+    public static void main(String[] args) {
+
+        int sisiPersegi = 5;
+        //rumus persegi sisi x sisi
+        int rumus_persegi = sisiPersegi * sisiPersegi;
+        System.out.println("hasil : "+ rumus_persegi);
+    }
+}
