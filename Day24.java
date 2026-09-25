236dfaecde7677973f0d68e761109fb990c5aaa8
irwangga02-sketch <irwangga02@gmail.com>
2026-09-25 19:33:21 +0800

    Create Day24.java

---
+++ Day24.java
@@ -0,0 +1,12 @@
+public class Day24 {
+    
+    public static void main(String[] args) {
+
+        int P = 10;
+        int L = 5;
+        // rumus mencari luas persegi panjang = P(panjang) x L(lebar)
+        int Luas = P * L;
+        
+        System.out.println("luas persegi panjang : "+ Luas);
+    }
+}
